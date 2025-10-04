354acf146996586c6da110abf53a107a15992487
RAJKISHORE HUDUK <rajkishorehuduk@gmail.com>
2025-10-04 21:00:09 +0530

    Create counter.js
    Simple React Counter Component built with Functional Components and the useState Hook. Demonstrates fundamental concepts of component state management and unidirectional data flow in React.

---
+++ counter.js
@@ -0,0 +1,15 @@
+const CounterButton = () => {
+  const [count, setCount] = React.useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return React.createElement('div', null,
+    React.createElement('h1', null, 'Simple Click Counter'),
+    React.createElement('p', null, `You clicked the button ${count} times.`),
+    React.createElement('button', { onClick: handleClick }, 'Click Me!')
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('counter-demo')).render(React.createElement(CounterButton));

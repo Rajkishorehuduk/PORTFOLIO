79a9d17a6f690e7042e6cb7b6b587de0ed1c60aa
RAJKISHORE HUDUK <rajkishorehuduk@gmail.com>
2025-10-05 22:56:24 +0530

    Update counter.js

--- counter.js
+++ counter.js
@@ -4,7 +4,6 @@ const CounterButton = () => {
   const handleClick = () => {
     setCount(count + 1);
   };
-
   return React.createElement('div', null,
     React.createElement('h1', null, 'Simple Click Counter'),
     React.createElement('p', null, `You clicked the button ${count} times.`),

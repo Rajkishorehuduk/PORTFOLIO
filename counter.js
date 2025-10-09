110a0b8688bf224eb20dc26b35cda3286b89fa92
RAJKISHORE HUDUK <rajkishorehuduk@gmail.com>
2025-10-09 20:31:44 +0530

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,13 +1,17 @@
 const CounterButton = () => {
   const [count, setCount] = React.useState(0);
+  const [isAnimating, setIsAnimating] = React.useState(false);
 
   const handleClick = () => {
+    setIsAnimating(true);
     setCount(count + 1);
+    setTimeout(() => setIsAnimating(false), 150);
   };
+
   return React.createElement('div', null,
     React.createElement('h1', null, 'Simple Click Counter'),
     React.createElement('p', null, `You clicked the button ${count} times.`),
-    React.createElement('button', { onClick: handleClick }, 'Click Me!')
+    React.createElement('button', { onClick: handleClick, className: `btn btn-primary ${isAnimating ? 'animate-click' : ''}` }, 'Click Me!')
   );
 };
 
